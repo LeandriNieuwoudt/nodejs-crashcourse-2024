13aeb9236a86eeeb4e4a8336e782ed0db47f4e97
LeandriNieuwoudt <leandri@mohara.co>
2024-07-06 15:21:18 +0200

    add user id param

--- server2.js
+++ server2.js
@@ -12,6 +12,18 @@ const server = createServer((req, res) => {
     res.setHeader("Content-Type", "application/json");
     res.write(JSON.stringify(users));
     res.end();
+  } else if (req.url.match(/\/api\/users\/([0-9]+)/) && req.method === "GET") {
+    const id = req.url.split("/")[3];
+    const user = users.find((user) => user.id === parseInt(id));
+    res.setHeader("Content-Type", "application/json");
+    if (user) {
+      res.statusCode = 200;
+      res.write(JSON.stringify(user));
+      res.end();
+    } else {
+      res.statusCode = 404;
+      res.end(JSON.stringify({ message: "User not found" }));
+    }
   } else {
     res.writeHead(404, { "Content-Type": "application/json" });
     res.end("Not Found");
